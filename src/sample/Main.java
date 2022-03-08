3bf98f95139c5300507ea918cbec1a9d17625370
BrynjolfurS <brynjolfurstein@gmail.com>
2022-03-08 18:25:16 +0000

    Added .idea/workspace to .gitignore + changed default screen size for app

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,10 +12,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Search.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
