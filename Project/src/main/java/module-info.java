97e7c78402f2dfe5c8dfe71f0fbbcc8f97dde2ef
Arnar-Sig <enteko5@gmail.com>
2022-03-08 15:36:15 +0000

    eftir copy a daemi fra kennara

--- Project/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module project8d.daytours {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens project8d.daytours to javafx.fxml;
-    exports project8d.daytours;
-}
