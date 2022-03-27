ff3e0fa454984cdc5787f5455c41c9969d9860ac
Arnar-Sig <enteko5@gmail.com>
2022-03-27 18:45:48 +0000

    baett vid test case

--- src/main/java/sample/daytoursnyttsdk/DayTourTests.java
+++ src/main/java/sample/daytoursnyttsdk/DayTourTests.java
@@ -5,6 +5,8 @@ import org.junit.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -34,7 +36,7 @@ public class DayTourTests {
     }
 
     @Test
-    public void testUIsearch(){
+    public void testUIsearchGeneral(){
         ArrayList<String> dummyActivities = new ArrayList<>(); dummyActivities.add("Gongutur"); dummyActivities.add("Hjolaferd");
         SearchModel dummySM = new SearchModel("Reykjavik", 0, 180, 0, 5, dummyActivities,
                 0, 10000, 5, LocalDate.of(2022, 01, 01),
@@ -50,4 +52,38 @@ public class DayTourTests {
         assertNotEquals(0, dummyUtkoma.size());
         assertTrue(dummyUtkoma.get(0) instanceof String);
     }
+
+    @Test
+    public void testUIsearchSpecific(){
+        ArrayList<String> dummyActivities = new ArrayList<>(); dummyActivities.add("Gonguferd");
+        SearchModel dummySM = new SearchModel("Reykjavik", 120, 120, 3, 3, dummyActivities,
+                1000, 1000, 5, LocalDate.of(2022, 04, 07),
+                LocalDate.of(2022, 04, 07), false);
+        DB databaseConnection = new DB();
+        ArrayList<String> dummyUtkoma = new ArrayList<>();
+        try {
+            dummyUtkoma = databaseConnection.searchDayTours(dummySM);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String[] list = dummyUtkoma.get(0).split(",");
+        /*
+        System.out.println(dummyUtkoma);
+        for(int i=0; i<list.length;i++){
+            System.out.println(list[i]);
+        }
+         */
+        assertEquals("Gonguferd um Reykjavik", list[0]);
+        assertEquals(" 2022-04-07", list[1]);
+        assertEquals( " " + 1000, list[2]);
+        assertEquals(" Reykjavik", list[3]);
+        assertEquals(" " + 15, list[4]);
+        assertEquals(" Gonguferd", list[5]);
+        assertEquals(" " + 3, list[6]);
+        assertEquals(" " + 0,list[7]);
+        assertEquals(" " + 120, list[8]);
+
+    }
+
 }
