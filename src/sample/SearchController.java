c8879c9620b0c9c2b8e78afc00c583514d771c92
BrynjolfurS <brynjolfurstein@gmail.com>
2022-03-27 16:37:17 +0000

    Fall í controller lagar crash þegar leitað er með tóma TextFields.

--- src/sample/SearchController.java
+++ src/sample/SearchController.java
@@ -1,14 +1,19 @@
 package sample;
 
 import java.net.URL;
+import java.sql.Array;
 import java.sql.Date;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 
 /***********************************************************
  * Lýsing:
@@ -17,6 +22,9 @@ import javafx.scene.control.*;
  ***********************************************************/
 public class SearchController implements Initializable {
 
+    // Viðmótshlutir
+    @FXML
+    public VBox fxSearchParameters;
     @FXML
     public TextField fxSpotsLeft;
     @FXML
@@ -51,14 +59,26 @@ public class SearchController implements Initializable {
     private CheckBox fxActivities5;
 
 
-
     // geymslubreytur
     private DB databaseConnection;
     private DayTours dt;
+    private ArrayList<TextField> numSearchParams;
+
+    // Constants
+    static final int DEFAULT_MIN = 0;
+    static final int PRICE_MAX = 1000000;
+    static final int DIFFICULTY_MAX = 5;
+    static final int DURATION_MAX = 888;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         databaseConnection = new DB();
+        numSearchParams = new ArrayList<>();
+        Collections.addAll(numSearchParams,
+                fxSpotsLeft,
+                fxMinDuration, fxMinDifficulty, fxMinimumPrice,
+                fxMaxDuration, fxMaxDifficulty, fxMaximumPrice);
+
         /*
         try {
             databaseTest.tengjastDB();
@@ -67,7 +87,6 @@ public class SearchController implements Initializable {
         }
 
          */
-
     }
 
     public void searchButton(ActionEvent actionEvent) {
@@ -78,10 +97,15 @@ public class SearchController implements Initializable {
 
         Date fra = new Date(122, 4, 1);
         Date til = new Date(122, 4, 5);
-        SearchModel sm = new SearchModel(fxLocation.getText(), Integer.parseInt(fxMinDuration.getText()), Integer.parseInt(fxMaxDuration.getText()),
-                 Integer.parseInt(fxMinDifficulty.getText()), Integer.parseInt(fxMaxDifficulty.getText()), fxActivities1.getText(), Integer.parseInt(fxMinimumPrice.getText()),
-                 Integer.parseInt(fxMaximumPrice.getText()), Integer.parseInt(fxSpotsLeft.getText()), fra, til, fxHotelPickup.isSelected());
 
+        // BINNI NOTES - HENDA ÚT
+        // fxObject.getText().isEmpty() getur tékkað hvort textField er tómur
+
+        int num_params[] = getNumParams();
+
+        SearchModel sm = new SearchModel(fxLocation.getText(), num_params[1], num_params[4],
+                 num_params[2], num_params[5], fxActivities1.getText(), num_params[3],
+                num_params[6], num_params[0], fra, til, fxHotelPickup.isSelected());
         /*
         //SEARCHMODEL DÆMI SEM VIRKAR
         Date fra = new Date(122, 4, 1);
@@ -104,5 +128,31 @@ public class SearchController implements Initializable {
         dt.updateSearchModel(sm);
         //"2022-04-03 17:00:000"
     }
-}
 
+    // Sækir gögn úr TextFields. Ef strengur er tómur þá er
+    // skilað sjálfgefnu há- eða lággildi
+    private int[] getNumParams() {
+        int p[] = new int[7];
+        for (int i = 0; i < numSearchParams.size(); i++) {
+            TextField text = numSearchParams.get(i);
+            if (!text.getText().isEmpty()) {
+                p[i] = Integer.parseInt(text.getText());
+            }
+            else {
+                if (i < 4) {
+                    p[i] = DEFAULT_MIN;
+                }
+                else if (i == 4) {
+                    p[i] = DURATION_MAX;
+                }
+                else if (i == 5){
+                    p[i] = DIFFICULTY_MAX;
+                }
+                else {
+                    p[i] = PRICE_MAX;
+                }
+            }
+        }
+        return p;
+    }
+}
